f3558a78c12b84e912b23817a77af0e9c6656908
pangwa <pangwa@gmail.com>
2013-07-27 18:13:19 +0800

    implement the sign method.

--- bae-message.js
+++ bae-message.js
@@ -1,6 +1,49 @@
 
 
-exports.BaeMessage = function(opt){
+var http = require('http');
+var crypto = require('crypto');
+var queryString = require('querystring');
 
+var md5 = crypto.createHash('md5');
+//
+// check the sign algorithm at:
+// http://developer.baidu.com/wiki/index.php?title=docs/cplat/mq/sign
+// method : POST or GET
+function signRequest(method, url, params, secret){
+    //
+    // normalize request method
+    method = method.toUpperCase();
+    if(!(method === 'POST' || method === 'GET')){
+        throw new Error('Request method should either Post or Get and you passed "' + method + '"');
+    }
+    params = params || {};
+    //
+    // form the parameters string
+    var paramStr = '';
+    for(v in params){
+        paramStr += v + '=' + params[v];
+    }
+    var basicString = method + paramStr + secret;
+    var encoded = queryString.escape(basicString);
+    console.log(encoded);
+    md5.update(encoded);
+    var sign = md5.digest('hex');
+    var body = '';
+    for(v in params){
+        body += queryString.escape(v) + '=' + queryString.escape(params[v]);
+    }
+    body += 'sign=' + sign; //append the signature
+    return body;
 }
+
+//
+// {
+//    apiKey : "", //also the client id
+//    secret : "",
+// }
+module.exports = function(opt){
+    opt = opt || {};
+}
+
+module.exports.signRequest = signRequest;
 
