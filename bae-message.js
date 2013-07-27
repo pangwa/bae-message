1b87887543362607fbd47036217454e52a80bf7b
pangwa <pangwa@gmail.com>
2013-07-27 23:20:41 +0800

    make send to one email address work

--- bae-message.js
+++ bae-message.js
@@ -1,10 +1,28 @@
 
-
 var http = require('http');
 var crypto = require('crypto');
-var queryString = require('querystring');
+var url = require('url');
+
 
 var md5 = crypto.createHash('md5');
+
+
+var BaeBaseUrl = 'bcms.api.duapp.com';
+var BasePath = '/rest/2.0/bcms/';
+
+//
+// php like URL encode
+function URLEncode(str) {
+  // http://kevin.vanzonneveld.net
+  str = (str + '').toString();
+
+  // Tilde should be allowed unescaped in future versions of PHP (as reflected below), but if you want to reflect current
+  // PHP behavior, you would need to add ".replace(/~/g, '%7E');" to the following.
+  return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').
+  replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+');
+}
+
+
 //
 // check the sign algorithm at:
 // http://developer.baidu.com/wiki/index.php?title=docs/cplat/mq/sign
@@ -24,12 +42,12 @@ function signRequest(method, url, params, secret){
         paramStr += v + '=' + params[v];
     }
     var basicString = method + url + paramStr + secret;
-    var encoded = queryString.escape(basicString);
+    var encoded = URLEncode(basicString);
     md5.update(encoded);
     var sign = md5.digest('hex');
     var body = '';
     for(v in params){
-        body += queryString.escape(v) + '=' + queryString.escape(params[v]) + '&';
+        body += URLEncode(v) + '=' + URLEncode(params[v]) + '&';
     }
     body += 'sign=' + sign; //append the signature
     return body;
@@ -42,7 +60,55 @@ function signRequest(method, url, params, secret){
 // }
 module.exports = function(opt){
     opt = opt || {};
+
+    function prepareRequest(opts){
+        var req = http.request(opts, function(res){
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+            res.on('data', function (chunk) {
+                console.log('BODY: ' + chunk);
+            });
+        });
+
+        req.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+        return req;
+    };
+
+    this.mail = function (from, to, subject, message){
+        var method = 'mail';
+        var client_id = opt.key;
+        var srvUrl = BasePath + opt.queue;
+        var timeStamp = Math.round(+new Date()/ 1000);
+        var params = {
+            address : to,
+            mail_subject : subject,
+            method : 'mail',
+            client_id : client_id,
+            timestamp : timeStamp,
+            from: from,
+            message : message,
+        }
+        var reqURL = 'http://' + BaeBaseUrl + srvUrl;
+        var body = signRequest('POST', reqURL, params, opt.secret);
+        var options = {
+            hostname: BaeBaseUrl,
+            path: srvUrl,
+            method: 'POST',
+            headers : {
+                "User-Agent" : "bae-message",
+                "Content-Length" : body.length,
+                "Content-Type" : "application/x-www-form-urlencoded"
+            }
+        };
+        console.log('sending request to ' + reqURL);
+        console.log('with body' + body);
+        var req = prepareRequest(options);
+        req.write(body);
+        req.end();
+    }
 }
 
 module.exports.signRequest = signRequest;
-
